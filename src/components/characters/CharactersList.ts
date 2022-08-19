a374ac019763ce2b37c1014b06c48a2449fc46f1
zajdawid <zajdawid@protonmail.com>
2022-08-19 08:05:37 +0200

    fix(rwd styles)

--- src/components/characters/CharactersList.ts
+++ src/components/characters/CharactersList.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export default styled.ul`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
   gap: ${({ theme }) => theme.space[2]};
   margin: ${({ theme }) => theme.space[2]} 0;
 `;
